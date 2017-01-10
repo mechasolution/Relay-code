73815f77144946e33b42d1a66ce316da846d1373
김재균 <worbs0604@gmail.com>
2017-01-10 14:24:54 +0900

    Add files via upload

---
+++ relay/relay.ino
@@ -0,0 +1,16 @@
+int relay = 9;
+int button = 2;
+
+void setup() {
+  pinMode(relay, OUTPUT);
+  pinMode(button, INPUT);
+}
+
+void loop() {
+  if (digitalRead(button) == HIGH) {
+    digitalWrite(relay, HIGH);
+  }
+  else {
+    digitalWrite(relay, LOW);
+  }
+}
